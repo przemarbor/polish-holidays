0683dc0008d06f6394c0790872d1f44f7b65bd96
przemarbor <marbor@prz.edu.pl>
2025-05-25 21:26:40 +0200

    A fix for: "You should depend on (emacs "24.1") if you need lexical-binding.".

--- polish-holidays.el
+++ polish-holidays.el
@@ -6,6 +6,7 @@
 ;; URL: https://github.com/przemarbor/polish-holidays
 ;; Version: 1.0.0
 ;; Keywords: calendar
+;; Package-Requires: ((emacs "24.1"))
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
